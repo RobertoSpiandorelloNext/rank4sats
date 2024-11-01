a7b9f8950443191f9c77ce569f1bf240b50210ab
Roberto Spiandorello <roberto.spiandorello.ir@outlook.com>
2024-11-01 20:27:48 -0300

    webhook fixings

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,53 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import crypto from 'crypto';
-
-const SIGNING_SECRET = process.env.SIGNING_SECRET || ''; // Seu segredo de assinatura
-
-// Função para verificar a assinatura
-function verifySignature(signature: string, payload: string, secret: string): boolean {
-  const [version, receivedSignature] = signature.split(',');
-  if (version !== 'v1') {
-    return false;
-  }
-
-  const hmac = crypto.createHmac('sha256', secret);
-  hmac.update(payload);
-  const expectedSignature = `v1,${hmac.digest('base64')}`;
-
-  const receivedBuffer = Buffer.from(receivedSignature, 'base64');
-  const expectedBuffer = Buffer.from(expectedSignature.split(',')[1], 'base64');
-
-  if (receivedBuffer.length !== expectedBuffer.length) {
-    return false;
-  }
-
-  return crypto.timingSafeEqual(receivedBuffer, expectedBuffer);
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const signature = req.headers['webhook-signature'];
-      if (!signature || Array.isArray(signature)) {
+      if (!signature) {
         return res.status(400).json({ error: 'Missing or invalid signature header' });
       }
 
       // Acessa o corpo da requisição diretamente
       const body = await req.body;
 
-      // Verifica a assinatura
-      const isValidSignature = verifySignature(signature, JSON.stringify(body), SIGNING_SECRET);
-
-      if (!isValidSignature) {
-        return res.status(401).json({ error: 'Invalid signature' });
-      }
-
-      // Processa o evento se a assinatura for válida
-      console.log("Evento válido recebido:", body);
-
       return res.status(200).json({ message: 'Website registered successfully', body });
     } catch (error) {
-      console.error("Erro ao processar o evento:", error);
       return res.status(500).json({ error: 'Internal server error' });
     }
   } else {
