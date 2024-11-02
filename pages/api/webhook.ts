a3845ad74f5ad42bfd40e2b406a2f21a26a2f5a2
Roberto Spiandorello <roberto.spiandorello.ir@outlook.com>
2024-11-01 21:05:04 -0300

    env variables fix

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -5,7 +5,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME_2,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || '5432', 10),
   ssl: {
