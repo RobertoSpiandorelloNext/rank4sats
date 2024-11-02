bc365a3b87e666eb763be693893708d6e12df867
Roberto Spiandorello <roberto.spiandorello.ir@outlook.com>
2024-11-01 22:10:02 -0300

    db change

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -5,7 +5,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME_2,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || '5432', 10),
   ssl: {
@@ -13,21 +13,27 @@ const pool = new Pool({
   },
 });
 
-//
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Received request:', req.method, req.headers); // Log do método e cabeçalhos da requisição
+
   if (req.method === 'POST') {
     try {
       const signature = req.headers['webhook-signature'];
+      console.log('Webhook signature:', signature); // Log da assinatura do webhook
+
       if (!signature) {
         return res.status(400).json({ error: 'Missing or invalid signature header' });
       }
 
       // Acessa o corpo da requisição diretamente
       const body = req.body; // Não é necessário usar await aqui
+      console.log('Request body:', JSON.stringify(body)); // Log do corpo da requisição
 
       if (body.event_type === "payment.confirmed") {
+        console.log('Processing confirmed payment...'); // Log informativo sobre o processamento
+
         const client = await pool.connect();
+        console.log('Database connection established.'); // Log de conexão ao banco
 
         const insertQuery = `
           INSERT INTO payments (webhook_event)
@@ -36,18 +42,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         `;
 
         const values = [JSON.stringify(body)];
+        console.log('Executing query:', insertQuery, 'with values:', values); // Log da consulta SQL
+
         const result = await client.query(insertQuery, values);
+        console.log('Insert result:', result); // Log do resultado da inserção
+
         client.release();
-        
+        console.log('Database connection released.'); // Log de liberação da conexão
+
         const paymentId = result.rows[0].id;
         return res.status(200).json({ message: 'Confirmed payment registered successfully', paymentId });
       }
+      
+      console.log('Event triggered but not a confirmed payment.'); // Log informativo para outros eventos
       return res.status(200).json({ message: 'Event triggered but not a confirmed payment' });
     } catch (error) {
       console.error('Error processing request:', error); // Log do erro
       return res.status(500).json({ error: 'Internal server error' });
     }
   } else {
+    console.log('Method not allowed:', req.method); // Log do método não permitido
     return res.status(405).json({ error: 'Method not allowed' });
   }
 }
